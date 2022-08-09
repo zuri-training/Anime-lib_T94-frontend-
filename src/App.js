c864574d4ec42b628bdcca2365fa89529b97fb2d
Nedu <nedumrobert@gmail.com>
2022-08-09 15:28:01 -0400

    Revert "test"
    This reverts commit c62c1f773492823fd0ba1bb53d76eaeac23a89aa.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-// import Grid from '@mui/material/Grid'
-import HomePage from "./pages/home/home.page";
-import "./App.css";
+import Grid from '@mui/material/Grid'
+
+import './App.css';
 
 function App() {
-  return <HomePage />;
-  // return (
-  //     <Grid container direction="column">
-  //       <div>coming soon!</div>
-  //     </Grid>
-  // );
+  return (
+      <Grid container direction="column">
+        <div>coming soon!</div>
+      </Grid>
+  );
 }
 
 export default App;
